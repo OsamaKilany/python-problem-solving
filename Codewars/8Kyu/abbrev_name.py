767b1cd02e559033582b846574fd24c15f82a77b
osamaKilany <osamaalkilany00@gmail.com>
2026-07-05 07:29:39 +0300

    Abbreviate a Two Word Name

--- Codewars/8Kyu/abbrev_name.py
+++ Codewars/8Kyu/abbrev_name.py
@@ -1,9 +1,16 @@
 def abbrev_name(name):
+    name = name.title().split()
+    return f"{name[0][0]}.{name[1][0]}"
+
+
+
+
+"""
     name = name.title().replace(" ", ".")
     for letter in name:
         if letter.islower():
             name = name.replace(letter, "")
     return name
-
+"""
 
 print(abbrev_name("Pinto Bean"))
