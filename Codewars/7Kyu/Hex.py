08c49540f1097d8d34879c7c484dc6dc95ef1199
osamaKilany <osamaalkilany00@gmail.com>
2026-07-19 11:41:21 +0300

    Find The Corrupted Hex Byte

--- Codewars/7Kyu/Hex.py
+++ Codewars/7Kyu/Hex.py
@@ -2,14 +2,13 @@ HEX = set("0123456789ABCDEF")
 
 def find_corrupted_byte(dump):
     for index in dump:
-        cur = set(index)
-        if not cur in HEX:
+        cunr = set(index)
+        print(cunr, HEX, sep="\n")
+
+        for _ in range(2):
+            if not cunr in HEX:
                 return dump.index(index)
-           
-    return -1
-
-
-
+            
 
 
 print(find_corrupted_byte(["48", "65", "6C", "6C", "6F"]))
