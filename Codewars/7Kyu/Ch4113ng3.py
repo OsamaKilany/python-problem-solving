ede03d98976a1ba13e4216109ef2d32de3a59e7e
osamaKilany <osamaalkilany00@gmail.com>
2026-07-20 11:00:21 +0300

    Ch4113ng3

--- Codewars/7Kyu/Ch4113ng3.py
+++ Codewars/7Kyu/Ch4113ng3.py
@@ -1,7 +1,16 @@
 def nerdify(txt):
-    d = "aAeEl"
-    n = "431"
-    return txt.replace(d, n)
+       
+    if 'l' in txt:
+        txt= txt.replace('l', '1')
+        
+    txt = txt.lower()
+    
+    if "e" in txt:
+        txt = txt.replace('e', '3')
+    if "a" in txt:
+        txt =txt.replace('a', '4')
+    
+    return txt.capitalize()
 
 
 
