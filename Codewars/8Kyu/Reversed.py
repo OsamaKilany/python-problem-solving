4b9a28e95e9c5cc8ba1fbfdaee71fabe92179c14
osamaKilany <osamaalkilany00@gmail.com>
2026-08-06 18:25:15 +0300

    Reversed Strings

--- Codewars/8Kyu/Reversed.py
+++ Codewars/8Kyu/Reversed.py
@@ -1,9 +1,21 @@
 def solution(string):
-    
-    return string[::-1]
 
-    # Anothor solution
-    # return ''.join(reversed(string))
 
     
+    # Frist solution
+       return string[::-1]
+
+    
+    # Second solution
+      #reversed_chars = []
+      #for char in range(len(string)-1,-1,-1):
+         #reversed_chars.append(string[char])
+      #return ''.join(reversed_chars)
+    
+    
+    # Theird solution
+       #return ''.join(reversed(string))
+
+   
+
 print(solution("world"))
