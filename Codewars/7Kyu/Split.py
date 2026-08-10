51291153857568c9e99f3c071fb7cfa5894f33cf
osamaKilany <osamaalkilany00@gmail.com>
2026-08-10 14:44:30 +0300

    Split by Mask

--- Codewars/7Kyu/Split.py
+++ Codewars/7Kyu/Split.py
@@ -1,13 +1,19 @@
 def split_by_mask(strng, mask):
+
+    
+    if sum(mask) != len(strng):
+         return
+
     split_list = []
     tem = strng
-    if sum(mask) != len(strng):
-        return []
+    
     for i in mask:
         split_list.append(tem[:i])
-        tem = tem.replace(tem[:i], "", 1)
+        tem = tem[i:]
             
     return split_list  
     
+
+
 
 print(split_by_mask("3B0b38d2DDe04C8eF6AC56E3A6", (15, 3, 1, 7)))   
